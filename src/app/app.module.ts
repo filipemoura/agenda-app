2c60eff6d3c1f8ac3f2ef603526b21b9b1074933
Filipe Alves <filipe.alves@sefin.fortaleza.ce.gov.br>
2021-04-17 07:09:11 -0300

    Melhoria de layout da tela de contato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +30,10 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatInputModule,
     HttpClientModule,
     ReactiveFormsModule,
+    MatTabsModule,
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
     BrowserAnimationsModule
   ],
   providers: [
